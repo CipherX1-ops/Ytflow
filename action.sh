8b989c2f61cb9e2f464f2ea6ee59e3555a8c5b56
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-07-30 17:29:33 +0430

    Create action.sh

---
+++ action.sh
@@ -0,0 +1,6 @@
+branch=main
+git clone -b $branch https://github.com/CipherX1-ops/YouTubedl /root/CipherX1-ops
+cp yt/.env /root/CipherX1-ops/.env
+cd /root/CipherX1-ops
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t yt
+docker run --privileged --env-file .env --rm -i yt
